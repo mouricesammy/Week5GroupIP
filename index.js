e19e1175ed781c0f4e9c9a3c3452c492ea2a4a91
Mourice Oronje <moronje@safaricom.co.ke>
2023-02-20 22:00:55 +0300

    added update and delete

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const router = express.Router();
+
+const Schema = mongoose.Schema
+
+var employeeSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: String,
+  email: String,
+  status: String
+}, { collection: 'employees' })
+var employeeData = mongoose.model('employees', employeeSchema)
 
 
 // Define the connection string
@@ -30,3 +41,35 @@ app.get('/', function (req, res) {
 app.listen(PORT ,()=> {
     console.log(`Server is running on port ${PORT}`)
 });
+
+
+//update
+
+router.patch('/:id', async function (req, res) {
+  try {
+      const id = req.params.id;
+
+      const data = req.body;
+
+      const updatedData = await employeeData.findByIdAndUpdate(id, data);
+      res.status(200).json(updatedData);
+  } catch (error) {
+      res.status(400).json({ message: error.message });
+  }
+});
+
+//delete
+
+router.delete('/:id', async function (req, res) {
+  try {
+      const id = req.params.id;
+      const deletedData = await employeeData.findByIdAndDelete(id);
+      res.status(200).json(deletedData);
+  } catch (error) {
+      res.status(400).json({ message: error.message });
+  }
+});
+
+
+
+module.exports = router;
